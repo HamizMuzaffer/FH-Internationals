fc2cfe492dba75541c690dd2d167a62017faa53c
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-10-07 23:37:19 +0500

    configs-added

---
+++ src/components/statics/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div>
+      <img src="/logo-new.svg" alt="" width={100} height={100} />
+      </div>
+    </div>
+  )
+}
+
+export default Header
