c7cf5c7c7373733a7b960f41a7fda65dacafe447
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-10-20 04:05:18 +0500

    animations-added

--- src/components/dashboard/Corporate/Deliver.tsx
+++ src/components/dashboard/Corporate/Deliver.tsx
@@ -1,9 +1,17 @@
 import { SignupDrawer } from "@/components/statics/SignupDrawer";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import AOS from "aos";
+// Scroll Animation
+AOS.init();
+import "aos/dist/aos.css";
+import React from "react";
+
 
 const Deliver = () => {
-
+    React.useEffect(() => {
+        AOS.init({ duration: 1000, once: false });
+      }, []);
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
     const handleClick = () => {
@@ -15,13 +23,21 @@ const Deliver = () => {
     };
 
   return (
-    <div className="flex lg:flex-row flex-col min-h-[600px]">
-      <div className="lg:w-1/2 w-full flex-col justify-center items-center">
+    <div className="flex lg:flex-row flex-col-reverse min-h-[600px]">
+        <div className="flex lg:w-1/2 w-full px-12  h-full justify-center items-center">
+        <img 
+          data-aos= "flip-left"
+          src="/world.svg"
+          alt=""
+          className="h-full w-[600px] object-contain"
+        />
+      </div>
+      <div className="lg:w-1/2 w-full flex-col justify-center items-center" data-aos="fade-up">
         <h3 className="font-ubold px-12 text-2xl lg:text-5xl">
           Where in the world you want to deliver today?
         </h3>
         <div className="h-1 w-44 mx-12 my-2 bg-blue-800"></div>
-        <p className="text-justify px-12 py-2 text-xl">
+        <p className="text-justify px-12 py-2 text-lg">
           Register now at ShopandShip.com and you鈥檒l get 30 physical shopping
           addresses worldwide. Your online shopping orders will be received at
           any of the global addresses you choose. We will then deliver to you in
@@ -36,13 +52,7 @@ const Deliver = () => {
         </Button>
       </div>
 
-      <div className="flex lg:w-1/2 w-full px-12  h-full justify-center items-center">
-        <img
-          src="/world.svg"
-          alt=""
-          className="h-full w-[600px] object-contain"
-        />
-      </div>
+      
       <SignupDrawer isOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
     </div>
     );
